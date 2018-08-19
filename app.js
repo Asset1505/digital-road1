d10d437461402b5dc1c819b45b0e145ec84fd52f
Jacob <jacob@MacBook-Pro-Jacob.local>
2018-08-19 08:34:03 +0600

    Made some changes

--- app.js
+++ app.js
@@ -3,42 +3,70 @@ var bodyParser = require("body-parser");
 var multer = require("multer");
 var fs = require("fs");
 var ejs = require('ejs');
-var mongoose = require('mongoose');
 var path = require('path');
 var Lazy = require('lazy');
 var csv = require('node-csv').createParser();
+const http = require('http');
+//var io = require('socket.io')(http);
+var assert = require('assert');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var autoIncrement = require('mongoose-auto-increment');
 
 var port = process.env.PORT || 3000
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/polyglotdev-test', { useNewUrlParser: true });
+let trackingState = {}
 
-var holeData = new mongoose.Schema({
-  data:{
+mongoose.Promise = global.Promise;
+var url = 'mongodb://localhost:27017/polyglotdev-test';
+mongoose.connect(url, { useNewUrlParser: true });
+autoIncrement.initialize(mongoose.connection);
+
+global.holeData = new mongoose.Schema({
+
     lat: {
-      type: String
+      type: String,
+      id: { type: Schema.Types.ObjectId, ref: 'lat' }
     },
     lon: {
-      type: String
+      type: String,
+      id: { type: Schema.Types.ObjectId, ref: 'lon' }
     },
     width: {
-      type: String
+      type: String,
+      id: { type: Schema.Types.ObjectId, ref: 'width' }
     },
     length: {
-      type: String
+      type: String,
+      id: { type: Schema.Types.ObjectId, ref: 'length' }
     },
     area: {
-      type: String
+      type: String,
+      id: { type: Schema.Types.ObjectId, ref: 'area' }
     },
     level: {
-      type: String
+      type: String,
+      id: { type: Schema.Types.ObjectId, ref: 'level' }
     },
     price: {
+      type: String,
+      id: { type: Schema.Types.ObjectId, ref: 'price' }
+    },
+    imagePath: {
       type: String
-    }
-  }
+    },
+    id: { type: Schema.Types.ObjectId, ref: 'Photo' },
+
 });
-//schema.set('toObject', { getters: true });schema.set('toObject', { getters: true });
+
+holeData.plugin(autoIncrement.plugin, 'lat');
+holeData.plugin(autoIncrement.plugin, 'lon');
+holeData.plugin(autoIncrement.plugin, 'width');
+holeData.plugin(autoIncrement.plugin, 'length');
+holeData.plugin(autoIncrement.plugin, 'area');
+holeData.plugin(autoIncrement.plugin, 'level');
+holeData.plugin(autoIncrement.plugin, 'price');
 
 var Hole = mongoose.model("Hole", holeData);
 var app = express();
@@ -48,12 +76,9 @@ var upload = multer({
     storage: multer.memoryStorage()
 });
 
-
-app.set('view engine', 'ejs');
-
-//app.use('/', routes);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/uploads'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -61,12 +86,14 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/', function(req, res){
-  console.log('Works');
-  res.render('index');
-});
+//const server = require('http').Server(app);
+// socket.io
+const io = require('socket.io').listen(server);
+// handling socket.io requests
 
-//upload.single('test')     -----------before
+app.get('/', function(req, res){
+	res.sendFile('index.html', { root: __dirname });
+});
 
 app.post('/', upload.single('test'), function (req, res, next) {
   var txt1 = req.file.buffer.toString();
@@ -75,7 +102,6 @@ app.post('/', upload.single('test'), function (req, res, next) {
   var imgName = 'hole-' + datetime + '.jpg';
 
   var arr = txt1.split(";").map(val => val);
-  //console.log(arr[0]);
   var data = arr[arr.length-1];
   var buf = new Buffer(data, 'base64');
   console.log('lat: ');
@@ -92,33 +118,17 @@ app.post('/', upload.single('test'), function (req, res, next) {
   console.log(arr[5]);
   console.log('price: ');
   console.log(arr[6]);
-  console.log('b64: ');
-  //console.log(arr[7]);
-  console.log(typeof req.file.lat);
 
-  var arr1 = [];
-  for (var i = 0; i < 81; ++i) {
-    arr1.push({ val: i, arr: [0, 1, 2, 3, 4, 5] });
-  }
+  var myData = new Hole({ lat: arr[0], lon: arr[1], width:arr[2], length:arr[3], area:arr[4],
+    level:arr[5], price:arr[6], imagePath: 'http://localhost:3000/' + imgName});
 
-  var myData = new Hole({data: { lat: arr[0], lon: arr[1], width:arr[2], length:arr[3], area:arr[4],
-    level:arr[5], price:arr[6], img: ('./uploads/' + imgName)}});
-/*
-  Hole.create({ data: arr }).
-  then(doc => {
-    console.log(doc);
-    process.exit(0);
-  });
-*/
   myData.save()
-  /*
     .then(item => {
       res.send("item saved to database");
     })
     .catch(err => {
       res.status(400).send("unable to save to database");
     });
-  */
 
   fs.writeFile('./uploads/' + imgName, buf, function (err) {
     if (err) {
@@ -126,6 +136,10 @@ app.post('/', upload.single('test'), function (req, res, next) {
     }
     res.end('Success!');
   });
+});
+
+app.post('/location', function(req, res) {
+  io.emit("location:changed", "req.body");
 });
 
 app.get('/images', function(req, res) {
@@ -137,40 +151,13 @@ app.get('/images', function(req, res) {
   });
 });
 
+app.get('/sendData', function(req, res, data){
+  res.render('index', );
+});
+
 app.getImages = function(callback, limit) {
   Hole.find(callback).limit(limit);
 }
-
-/*
-app.post("/upload", upload.array("uploads", 12), function(req, res) {
-    var fileInfo = [];
-    for(var i = 0; i < req.files.length; i++) {
-        fileInfo.push({
-            "originalName": req.files[i].originalName,
-            "size": req.files[i].size,
-            "b64": new Buffer(fs.readFileSync(req.files[i].path)).toString("base64")
-        });
-        fs.unlink(req.files[i].path);
-    }
-    res.send(fileInfo);
-});
-*/
-
-/*
-app.post('/fileInConsole', upload.array('file', 12), function (req, res, next) {
-    console.log(req.files);
-    var now = new Date();
-    var datetime = now.getFullYear() + '-' + now.getMonth() + '-' + now.getDate() + '-' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-    var imgName = 'hole-' + datetime + '.jpg';
-    fs.writeFile('./fileInConsole/' + imgName, function (err) {
-      if (err) {
-        //console.log('Error');
-        return next(err);
-      }
-    res.send("done");
-  });
-});
-*/
 
 var server = app.listen(port, function() {
     console.log("Listening on port %s...", server.address().port);
